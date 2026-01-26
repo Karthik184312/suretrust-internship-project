ae916288596b3087a6d4d3791fd95a60cda1f5c4
Karthik N R <karthikkarthi6837@gmail.com>
2026-01-26 15:49:28 +0530

    Update LED_blinks_based_on_duty_cycle.ino

--- LED_blinks_based_on_duty_cycle.ino
+++ LED_blinks_based_on_duty_cycle.ino
@@ -1,22 +1,35 @@
-
 int t = 1000;
 float dc = 0.0;
 int ton = 0;
 int toff = 0;
+
 void setup() {
-  pinMode(6, OUTPUT);
+  pinMode(8, OUTPUT);
   Serial.begin(9600);
 }
+
+bool valid = false;
+
 void loop() {
+
   if (Serial.available()) {
     dc = Serial.parseFloat();
+    valid = true;
+
     ton = dc * t;
     toff = t - ton;
-    Serial.println(ton); 
+    Serial.println(dc);
+    Serial.println(ton);
     Serial.println(toff);
   }
-  digitalWrite(6, HIGH);
+
+  if (!valid) {
+    digitalWrite(8, LOW);
+    return;
+  }
+
+  digitalWrite(8, HIGH);
   delay(ton);
-  digitalWrite(6, LOW);
+  digitalWrite(8, LOW);
   delay(toff);
 }
